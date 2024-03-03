7885859c2b22a0a822099864a60fbbbccde0c628
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-03-03 01:38:40 -0500

    created a form that allowed a user to input data and a route to render and handle that data.

--- express_server.js
+++ express_server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+function generateRandomString() {
+  let result = []
+  let alphanumeric = `123456789qwertyuiopasdfghjklzxcvbnm`
+  for (let i = 0; i < 6; i++) {
+    let random = Math.round(Math.random() * alphanumeric.length)
+    result.push(alphanumeric[random])
+  }
+  return result.join('')
+}
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -20,6 +29,13 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
