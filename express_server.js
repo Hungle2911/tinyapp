c192bbcbbac17e96d6ad599cf7d000fda6d32e67
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-03-07 00:02:06 -0500

    Modify _header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -27,11 +29,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase ,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 });
 app.post("/urls", (req, res) => {
   let id = generateRandomString();
@@ -39,7 +47,10 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${id}`);
 });
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id], 
+    username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 //Edit URL
@@ -55,6 +66,7 @@ app.post("/login", (req, res) => {
   res.cookie('username', submittedUsername)
   res.redirect('/urls')
 });
+
 //Delete URL
 app.post("/urls/:id/delete", (req, res) => {
   const idToDelete = req.params.id;
