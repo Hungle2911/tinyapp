852880ab8ed3a609fd00ca8e990c31c555b08a01
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-03-09 14:29:09 -0500

    Update the POST /login endpoint to look up the email address and give appropriate response

--- express_server.js
+++ express_server.js
@@ -50,13 +50,13 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase ,
-    username: users[req.cookies["user_id"]],
+    user_id: users[req.cookies["user_id"]],
   };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: users[req.cookies["user_id"]]
+    user_id: users[req.cookies["user_id"]]
   }
   res.render("urls_new", templateVars);
 });
@@ -69,7 +69,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
     longURL: urlDatabase[req.params.id], 
-    username: users[req.cookies["user_id"]] };
+    user_id: users[req.cookies["user_id"]] };
   res.render("urls_show", templateVars);
 });
 //Edit URL
@@ -81,9 +81,15 @@ app.post("/urls/:id", (req, res) => {
 });
 //Login
 app.post("/login", (req, res) => {
-  const submittedUsername = req.body.username
-  res.cookie('username', submittedUsername)
-  res.redirect('/urls')
+  const user = getUserByEmail(users, req.body.email)
+  if (!user) {
+    return res.status(403).send('User not found');
+  } else if (!(req.body.password === user.password)) {
+    return res.status(403).send('Incorrect password');
+  } else {
+    res.cookie('user_id', user.id);
+    res.redirect('/urls');
+  }
 });
 app.get('/login', (req, res) =>{
   res.render('login')
